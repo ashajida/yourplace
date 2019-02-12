e609b5ce61caebd367423c72e76818a5c0b7be77
ASHRAF K AJIDA <ashraf.ajida@yahoo.com>
2019-02-12 23:53:43 +0000

    setup

---
+++ database/migrations/2019_02_07_153933_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->decimal('price');
+            $table->integer('bedrooms');
+            $table->integer('bathrooms');
+            $table->string('postcode');
+            $table->mediumText('address');
+            $table->mediumText('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
